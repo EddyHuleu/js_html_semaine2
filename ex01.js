a65f48c3a4e3215df97fd9abef8c980ee212aaf3
Eddy Huleu <eddy.huleu@live.fr>
2016-12-10 18:24:21 +0100

    exercice01.7

--- ex01.js
+++ ex01.js
@@ -1,4 +1,19 @@
-var tab = new Array;
-tab.push(prompt('Quel est votre prénom?'));
-tab.push(prompt('Quel est votre nom?'));
-tab.push(prompt('Dans quelle ville habitez-vous?'));
+var age;
+var sexe;
+var nationalite
+age = prompt('Quel âge avez-vous ?');
+sexe = prompt('Etes vous un garçon ou une fille?')
+nationalite = prompt('De quelle nationalité êtes vous?')
+if(age >= 18 && sexe == "garçon"){
+      	alert('Vous êtes un garçon majeur ' +nationalite);
+}
+else if(age >= 18 && sexe == "fille"){
+   		alert('Vous êtes une fille majeur ' +nationalite)
+}
+else if(age <= 18 && sexe == "fille"){
+   		alert('Vous êtes une fille mineur ' +nationalite)
+}
+else if(age <= 18 && sexe == "garçon"){
+   		alert('Vous êtes un garçon mineur ' +nationalite)
+}
+
