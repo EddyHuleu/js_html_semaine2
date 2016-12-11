a5e2e091636d31a3a034fa3aa71aaa94579794d7
Eddy Huleu <eddy.huleu@live.fr>
2016-12-11 01:51:11 +0100

    exercice01.4

--- ex01.js
+++ ex01.js
@@ -1,19 +1,4 @@
-var age;
-var sexe;
-var nationalite
-age = prompt('Quel âge avez-vous ?');
-sexe = prompt('Etes vous un garçon ou une fille?')
-nationalite = prompt('De quelle nationalité êtes vous?')
-if(age >= 18 && sexe == "garçon"){
-      	alert('Vous êtes un garçon majeur ' +nationalite);
-}
-else if(age >= 18 && sexe == "fille"){
-   		alert('Vous êtes une fille majeur ' +nationalite)
-}
-else if(age <= 18 && sexe == "fille"){
-   		alert('Vous êtes une fille mineur ' +nationalite)
-}
-else if(age <= 18 && sexe == "garçon"){
-   		alert('Vous êtes un garçon mineur ' +nationalite)
-}
-
+var calcule1 = prompt("Entrez un nombre à virgule : ");
+var calcule2 = prompt("Entrez un deuxième nombre à virgule :");
+var resultat = parseInt(calcule1)*(calcule2);
+alert(resultat);
